67905effa526d061100032c249ab5a137ade8d90
Eadon <eadonj@gmail.com>
2013-04-18 15:32:43 -0700

    added links to other user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Railsdemo::Application.routes.draw do
   
+  get "users/show"
+
   resources :pins
 
-
   devise_for :users
+  match 'user/:id' => 'users#show', as: :user
+
+  get 'about' => 'pages#about'
 
   root :to => 'pins#index'
 
-  get 'about' => 'pages#about'
+  
 
 
   # The priority is based upon order of creation:
